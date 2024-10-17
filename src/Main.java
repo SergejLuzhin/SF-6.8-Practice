66855c7389111d2e23ec6f407803339c7833a0cc
Sergej <luzhins764@gmail.com>
2024-10-17 16:56:26 +0300

    task 683 added

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int task = 2;
+        int task = 3;
         switch (task){
             case 1:
                 Task6_8_1.execute();
                 break;
             case 2:
                 Task6_8_2.execute();
+                break;
+            case 3:
+                Task6_8_3.execute();
                 break;
         }
     }
