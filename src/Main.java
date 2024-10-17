92deb64e2b867b7984775278d8620280e2714b12
Sergej <luzhins764@gmail.com>
2024-10-17 16:49:39 +0300

    added 6_8_2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int task = 1;
+        int task = 2;
         switch (task){
             case 1:
                 Task6_8_1.execute();
+                break;
+            case 2:
+                Task6_8_2.execute();
                 break;
         }
     }
